f0af85023e0004c805deada8d7c53dcc59bf29a2
Lacey <lacey.griffith@g.austincc.edu>
2021-04-24 14:52:06 -0500

    change nav bar

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,17 +1,4 @@
-function openNav() {
-    document.getElementById("mySidenav").style.width = "120px";
+
+  const generatePage = () => {
+
   }
-  function closeNav() {
-    document.getElementById("mySidenav").style.width = "0";
-  }
-  // write into message to landing page //
-  document.addEventListener('DOMContentLoaded', function() {
-    writeText("#intro", "Hello! Thanks for stopping by, I look forward to connecting! -Lacey", 0, 85);
-});
-let writeText = function (target, message, index, interval) {
-    if (index < message.length) {
-        document.querySelector(target).innerHTML =
-            document.querySelector(target).innerHTML + message[index++];
-        setTimeout(function () { writeText(target, message, index, interval); }, interval);
-    }
-}
