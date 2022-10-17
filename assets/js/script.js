0e48fccaf08c2c51c51970f6f840116fd9313bec
Lacey <lacey.griffith04@gmail.com>
2022-10-16 19:06:46 -0500

    updating

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,24 @@
 
-  const generatePage = () => {
-
+function generateNavigation(){
+  if(typeof window.jQuery === undefined || !window.jQuery){
+    setTimeout(generateNavigation, 100);
+    return;
   }
+
+  let $ = window.jQuery;
+  let navigation = `
+  <!-- nav bar -->
+    <nav class="sidenav">
+        <ul class="justify-content-space-between">
+            <li><a href="index.html" class="allButtons">home</a></li>
+            <li><a href="about.html" class="allButtons">about</a></li>
+            <li><a href="projects.html" class="allButtons">projects</a></li>
+            <li><a href="contact.html" class="allButtons">contact</a></li>
+            <li><a href="/assets/images/LaceyGriffith_2022.pdf" class="allButtons" target="_blank">resume</a></li>
+        </ul>
+    </nav>
+  <!-- end nav bar -->`;
+  $('header').append(navigation)
+}
+
+generateNavigation();
