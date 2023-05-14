a363b6af506df216204d01af9a0c79e40be1e590
Lacey <lacey.griffith04@gmail.com>
2023-05-14 11:17:34 -0500

    syncing

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,3 @@
-
 function generateNavigation(){
   if(typeof window.jQuery === undefined || !window.jQuery){
     setTimeout(generateNavigation, 100);
@@ -10,15 +9,49 @@ function generateNavigation(){
   <!-- nav bar -->
     <nav class="sidenav">
         <ul class="justify-content-space-between">
-            <li><a href="index.html" class="allButtons">home</a></li>
-            <li><a href="about.html" class="allButtons">about</a></li>
-            <li><a href="projects.html" class="allButtons">projects</a></li>
-            <li><a href="contact.html" class="allButtons">contact</a></li>
-            <li><a href="assets/images/LaceyGriffith_2022.pdf" class="allButtons" target="_blank">resume</a></li>
+            <li><a href="#about" class="allButtons">about</a></li>
+            <li><a href="#projects" class="allButtons">projects</a></li>
+            <li><a href="#contact" class="allButtons">contact</a></li>
+            <li><a href="#resume" class="allButtons">resume</a></li>
         </ul>
     </nav>
   <!-- end nav bar -->`;
-  $('header').append(navigation)
+  if($('.sidenav').length == 0){
+    $('header').append(navigation);
+  }
+
+  $('nav ul li a').click(function(e){
+    e.preventDefault();
+    var dest=0;
+    if($(this.hash).offset().top > $(document).height()-$(window).height()){
+         dest=$(document).height()-$(window).height();
+    }else{
+         dest=$(this.hash).offset().top;
+    }
+    //go to destination
+    $('html,body').animate({scrollTop:dest}, 2500,'swing');
+  });
+}
+
+function projectSlider(){
+  let $ = window.jQuery;
+  if(typeof window.jQuery === undefined || !window.jQuery){
+    setTimeout(projectSlider, 100);
+    return;
+  }
+
+  $('body').append(`<script src="https://cdn.jsdelivr.net/npm/swiper@9/swiper-bundle.min.js"></script>`);
+  let parent = $('#projects-deployed .row');
+  parent.addClass('slick-container');
+
+  $('.slick-container').slick({
+    dots: true,
+    infinite: false,
+    speed: 300,
+    slidesToShow: 4,
+    slidesToScroll: 4,
+    });
 }
 
 generateNavigation();
+projectSlider();
