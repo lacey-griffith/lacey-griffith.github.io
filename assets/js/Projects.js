e430e56c9479f01cf06bbbf6e97c0b6bf1e2dd65
Lacey Hay <lacey.griffith04@gmail.com>
2024-12-22 15:16:20 -0600

    project section redesign

--- assets/js/Projects.js
+++ assets/js/Projects.js
@@ -1,73 +1,73 @@
 const tech = [
   {
-    language: 'HTML',
+    name: 'HTML',
     id: 0
   },
   {
-    language: 'CSS',
+    name: 'CSS',
     id: 1
   },
   {
-    language: 'BootStrap',
+    name: 'BootStrap',
     id: 2
   },
   {
-    language: 'React',
+    name: 'React',
     id: 3
   },
   {
-    language: 'JavaScript',
+    name: 'JavaScript',
     id: 4
   },
   {
-    language: 'jQuery',
+    name: 'jQuery',
     id: 5
   },
   {
-    language: 'Handlebars.js',
+    name: 'Handlebars.js',
     id: 6
   },
   {
-    language: 'Foundations',
+    name: 'Foundations',
     id: 7
   },
   {
-    language: 'APIs',
+    name: 'APIs',
     id: 8
   },
   {
-    language: 'IndexDB',
+    name: 'IndexDB',
     id: 9
   },
   {
-    language: 'Sequelize',
+    name: 'Sequelize',
     id: 10
   },
   {
-    language: 'Express.js',
+    name: 'Express.js',
     id: 11
   },
   {
-    language: 'MongoDB',
+    name: 'MongoDB',
     id: 12
   },
   {
-    language: 'Heroku',
+    name: 'Heroku',
     id: 13
   },
   {
-    language: 'Mongoose',
+    name: 'Mongoose',
     id: 14
   },
   {
-    language: 'MySQL2',
+    name: 'MySQL2',
     id: 15
   },
   {
-    language: 'GraphQL',
+    name: 'GraphQL',
     id: 16
   }, {
-    language: 'Node.js',
+    name: 'Node.js',
     id: 17
   }
 ];
@@ -75,7 +75,8 @@ const tech = [
 const projects = [
   {
     name: 'Flower Shop',
-    image: '/flower_shop.png',
+    handle: 'flower-shop',
+    image: 'assets/images/flower_shop.png',
     description: 'Flower Shop was created to enhance the online shopping experience for users wanting to purchase and send flowers to friends or loved ones. We combined simplicity with effeciency and the result is a user friendly, information driven, e-commerce flower shop. Users can browse by occasion, find details on products, add and remove items from their cart quickly and checkout seamlessly.',
     githubLink: 'https://github.com/hbbc248/the-flower-store',
     deploymentLink: 'https://flower-shop-project-3.herokuapp.com/',
@@ -83,7 +84,8 @@ const projects = [
   },
   {
     name: 'Namaste Fit',
-    image: '/namaste_fit.png',
+    handle: 'namaste-fit',
+    image: 'assets/images/namaste_fit.png',
     description: 'This application allows yoga enthusiasts to learn more about yoga poses. Our motivation for developing Namaste Fit is to improve the experiences surrounding yoga, personal journeys and education. Created by a team of 4, it is our first full stack application. We implemented Handlebars.js with little experience and successfully connected our backend database with our frontend functionality.',
     githubLink: 'https://github.com/lacey-griffith/yoga',
     deploymentLink: 'https://yoga-finish.herokuapp.com/',
@@ -91,7 +93,8 @@ const projects = [
   },
   {
     name: 'National Park Finder',
-    image: '/national_park_finder.png',
+    handle: 'national-park-finder',
+    image: 'assets/images/national_park_finder.png',
     description: 'National Park Finder was developed to assist travelers in planning a trip to a national park by giving the user information such as activies, location and current weather. I implemented Foundations, a framework never used before this project! It was challenging at first but dedicating time to the documentation and trial and error eventually led to success. Team members all worked together by communicating often, helping solve problems, and deploying the final project.',
     githubLink: 'https://github.com/glendonintendo/national-park-finder',
     deploymentLink: 'https://glendonintendo.github.io/national-park-finder/',
@@ -99,7 +102,8 @@ const projects = [
   },
   {
     name: 'Code Quiz',
-    image: '/code-quiz.png',
+    handle: 'code-quiz',
+    image: 'assets/images/code-quiz.png',
     description: 'Code Quiz allows students learning to code to test their knowledge of various compnents of Javascript. During this project I was learning to dynamically create objects from user input and to store data to use later. I was also getting my first exposure to implementing the use of local storage.',
     githubLink: 'github link',
     deploymentLink: 'deployment link',
@@ -107,15 +111,17 @@ const projects = [
   },
   {
     name: 'Password Generator',
-    image: '/PWgen_icon.png',
-    description: 'This application was created to help users generate a secure, randomized password based off their preferences for criteria. The length of the password and including lowercase, uppercase, numeric or special characters is all decided by the user. This project was my introduction to Javascript and the usefullness of the language became clearer as I progressed and learned more about the capabilities.',
+    handle: 'password-generator',
+    image: 'assets/images/PWgen_icon.png',
+    description: 'This application was created to help users generate a secure, randomized password based off their preferences for criteria. The length of the password and including lowercase, uppercase, numeric or special characters is all decided by the user. This project was my introduction to Javascript and the usefullness of the name became clearer as I progressed and learned more about the capabilities.',
     githubLink: 'https://github.com/lacey-griffith/friendly-parakeet',
     deploymentLink: 'https://lacey-griffith.github.io/friendly-parakeet/',
     techUsed: [tech[0], tech[1], tech[4]]
   },
   {
     name: 'Budget Tracker',
-    image: '/budget_tracker.png',
+    handle: 'budget-tracker',
+    image: 'assets/images/budget_tracker.png',
     description: 'Budget Tracker is an application to allow users to track their deposits and withdrawls while being online or off. As a PWA, progressive web application, it can be installed onto your phone or desktop and accessed at anytime. This project helped me understand that a PWA can truly increase user experience and meet their expectations of always having access to their applications.',
     githubLink: 'https://github.com/lacey-griffith/budget-tracker',
     deploymentLink: 'https://still-gorge-83604.herokuapp.com/',
@@ -123,6 +129,7 @@ const projects = [
   },
   {
     name: 'Tech Blog',
+    handle: 'tech-blog',
     image: '',
     description: 'This application was developed for bloggers to have a place to talk all things tech. It is versatile in the way that it could really be used for any blogger to start writing and sharing content. This project taught me how to utilize password security, session-storage and user authorization for specific activities (creating new posts, making comments) involving the server and database.',
     githubLink: 'https://github.com/lacey-griffith/tech-blog',
@@ -133,11 +140,36 @@ const projects = [
 
  function buildProjects () {
     let project_container = $('#projects-deployed .row');
+    project_container.addClass('swiper-wrapper');
+    project_container.parent().addClass('swiper mySwiper');
+    project_container.parent().append(`<div class="swiper-pagination"></div>`);
+
     
     projects.map((proj,i) => {
-      console.log(proj);
+      let projectCard =`
+      <div class='swiper-slide ${proj.handle}'>
+        <div class='card-inner'>       
+          <h1>${proj.name}</h1>
+          <img src='${proj.image}' alt=''/>
+          <p>${proj.description}</p>          
+          <div class='tech-used'></div>
+        </div>
+      </div>`;
+      $('.swiper-wrapper').append(projectCard);
 
-      let projectCard =``;
+      proj.techUsed.forEach(function(tech,e){
+        let value = tech.name
+        let item = `<span class='${value} tech-icon'>${tech.name}</span>`;
+        $(`div.${proj.handle} .tech-used`).append(item);
+      });
+    });
+
+    var swiper = new Swiper(".mySwiper", {
+      direction: "vertical",
+      pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+      },
     });
   }
 
